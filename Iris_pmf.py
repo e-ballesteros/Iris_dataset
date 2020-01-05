386b1fd1be1f084ae241589aa5f4d47bb4ef1830
Eduardo Ballesteros Abellán <ballesteros_eba@hotmail.com>
2020-01-05 20:28:57 +0100

    Minor changes

--- Iris_pmf.py
+++ Iris_pmf.py
@@ -8,13 +8,14 @@ iris = datasets.load_iris()
 data_matrix, class_vector = iris.data, iris.target
 data_matrix_int = (10*data_matrix[:, 0:4]).astype(int)
 
-unique_rows, pmf = pmf_multivariate(data_matrix_int)
+#unique_rows, pmf = pmf_multivariate(data_matrix_int)
 # Es absurdo, solo hay uno que se repita, HAY QUE CALCULAR PMF DE CADA FEATURE POR SEPARADO
-#unique_rows_1, pmf_1 = pmf_multivariate(data_matrix_int, 1)
+unique_rows_0, pmf_0 = pmf_multivariate(data_matrix_int, 0)
+unique_rows_1, pmf_1 = pmf_multivariate(data_matrix_int, 1)
 #unique_rows_2, pmf_2 = pmf_multivariate(data_matrix_int, 2)
 #unique_rows_3, pmf_3 = pmf_multivariate(data_matrix_int, 3)
 
 print(data_matrix_int)
-print(pmf)
+#print(pmf)
 
 
--- pmf_estimation_multivariate.py
+++ pmf_estimation_multivariate.py
@@ -4,8 +4,8 @@
 import numpy as np
 
 
-def pmf_multivariate(data_matrix):  # axis_index indicates axis to operate on
+def pmf_multivariate(data_matrix, matrix_axis):
     rows, columns = data_matrix.shape           # Number of rows and columns of data_matrix
-    unique_rows_array, pmf_vector = np.unique(data_matrix, axis=0, return_counts=True)
+    unique_rows_array, pmf_vector = np.unique(data_matrix, axis=matrix_axis, return_counts=True)
     return unique_rows_array, pmf_vector/rows
 
