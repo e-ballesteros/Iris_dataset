2480572a0fc545cbb8527ad39208e11ded77dc54
Eduardo Ballesteros Abellán <ballesteros_eba@hotmail.com>
2020-01-06 18:58:18 +0100

    Minor changes and pmf_estimation_multivariate corrected to use only one parameter

--- pmf_estimation_multivariate.py
+++ pmf_estimation_multivariate.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 
-def pmf_multivariate(data_matrix, matrix_axis):
+def pmf_multivariate(data_matrix):
     rows, columns = data_matrix.shape           # Number of rows and columns of data_matrix
-    unique_rows_array, pmf_vector = np.unique(data_matrix, axis=matrix_axis, return_counts=True)
+    unique_rows_array, pmf_vector = np.unique(data_matrix, axis=0, return_counts=True)
     return unique_rows_array, pmf_vector/rows
