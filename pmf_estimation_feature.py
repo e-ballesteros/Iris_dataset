364a19cc5bf12a0e23aa3d4859429cb22f7a291a
Eduardo Ballesteros Abellán <ballesteros_eba@hotmail.com>
2020-01-06 18:48:53 +0100

    pmf_estimation_feature added and written but not tested

---
+++ pmf_estimation_feature.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+import numpy as np
+
+
+def pmf_feature(data_matrix, matrix_axis):          # Axis of the feature
+    rows, columns = data_matrix.shape               # Number of rows and columns of data_matrix
+    unique_rows_array, pmf_vector = np.unique(data_matrix[:, matrix_axis], axis=0, return_counts=True)
+    return unique_rows_array, pmf_vector/rows
--- pmf_estimation_multivariate.py
+++ pmf_estimation_multivariate.py
@@ -8,4 +8,3 @@ def pmf_multivariate(data_matrix, matrix_axis):
     rows, columns = data_matrix.shape           # Number of rows and columns of data_matrix
     unique_rows_array, pmf_vector = np.unique(data_matrix, axis=matrix_axis, return_counts=True)
     return unique_rows_array, pmf_vector/rows
-
