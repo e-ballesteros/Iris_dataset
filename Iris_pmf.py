da4201664fafedd6c1ad5310bb40a7282d2949e8
Eduardo Ballesteros Abellán <ballesteros_eba@hotmail.com>
2020-01-07 17:21:12 +0100

    Computation of mutual information and change of the parameters of the mutual information function. Added comments to explain the code.

--- Iris_pmf.py
+++ Iris_pmf.py
@@ -13,10 +13,12 @@ from mutual_info import mutual_information
 
 iris = datasets.load_iris()
 data_matrix, class_vector = iris.data, iris.target
-data_matrix_int = (10*data_matrix[:, 0:4]).astype(int)
+data_matrix_int = (10*data_matrix[:, 0:4]).astype(int)      # Discretization of samples as integers
 
+# Computation of joint p.m.f. and unique rows inside the discrete matrix
 unique_rows, pmf = pmf_multivariate(data_matrix_int)
 
+# Computation of p.m.f. of each feature and unique elements inside each feature
 unique_rows_0, pmf_0 = pmf_feature(data_matrix_int, 0)
 unique_rows_1, pmf_1 = pmf_feature(data_matrix_int, 1)
 unique_rows_2, pmf_2 = pmf_feature(data_matrix_int, 2)
@@ -47,12 +49,16 @@ print('\nThe joint p.m.f. of the Iris dataset is:\n',
       '\ncorresponding to the unique rows:\n',
       unique_rows)
 
+# Computation of entropies of each feature
 print('\nThe entropy of the Sepal Length feature is: ', entropy(pmf_0))
 print('\nThe entropy of the Sepal Width feature is: ', entropy(pmf_1))
 print('\nThe entropy of the Petal Length feature is: ', entropy(pmf_2))
 print('\nThe entropy of the Petal Width feature is: ', entropy(pmf_3))
+
+# Computation of joint entropy
 print('\nThe joint entropy of the the Iris dataset is: ', entropy(pmf))
 
+# Computation of joint p.m.f. and unique rows inside the discrete matrix, of all the possible pairs of features
 unique_rows_0_1, pmf_0_1 = pmf_joint(data_matrix_int, 0, 1)
 unique_rows_0_2, pmf_0_2 = pmf_joint(data_matrix_int, 0, 2)
 unique_rows_0_3, pmf_0_3 = pmf_joint(data_matrix_int, 0, 3)
@@ -60,23 +66,18 @@ unique_rows_1_2, pmf_1_2 = pmf_joint(data_matrix_int, 1, 2)
 unique_rows_1_3, pmf_1_3 = pmf_joint(data_matrix_int, 1, 3)
 unique_rows_2_3, pmf_2_3 = pmf_joint(data_matrix_int, 2, 3)
 
-# WE NEED TO CREATE A TABLE IN ANOTHER FUNCTION WITH THE JOINT PMF AND THE MARGINAL PMFS IN ORDER
-print('\nThe mutual info of the Sepal Length and Sepal Width features is: ',
-      mutual_information(pmf_0_1, unique_rows_0_1, pmf_0, pmf_1))
+print('\nThe mutual information of the Sepal Length and Sepal Width features is: ',
+      mutual_information(pmf_0_1, unique_rows_0_1, pmf_0, unique_rows_0, pmf_1, unique_rows_1))
+print('\nThe mutual information of the Sepal Length and Petal Length features is: ',
+      mutual_information(pmf_0_2, unique_rows_0_2, pmf_0, unique_rows_0, pmf_2, unique_rows_2))
+print('\nThe mutual information of the Sepal Length and Petal Width features is: ',
+      mutual_information(pmf_0_3, unique_rows_0_3, pmf_0, unique_rows_0, pmf_3, unique_rows_3))
+print('\nThe mutual information of the Sepal Width and Petal Length features is: ',
+      mutual_information(pmf_1_2, unique_rows_1_2, pmf_1, unique_rows_1, pmf_2, unique_rows_2))
+print('\nThe mutual information of the Sepal Width and Petal Width features is: ',
+      mutual_information(pmf_1_3, unique_rows_1_3, pmf_1, unique_rows_1, pmf_3, unique_rows_3))
+print('\nThe mutual information of the Petal Length and Petal Width features is: ',
+      mutual_information(pmf_2_3, unique_rows_2_3, pmf_2, unique_rows_2, pmf_3, unique_rows_3))
 
-print('\nThe mutual info of the Sepal Length and Petal Length features is: ', mutual_information(pmf_0_2, pmf_0, pmf_2))
-print('\nThe mutual info of the Sepal Length and Petal Width features is: ', mutual_information(pmf_0_3, pmf_0, pmf_3))
-print('\nThe mutual info of the Sepal Width and Petal Length features is: ', mutual_information(pmf_1_2, pmf_1, pmf_2))
-print('\nThe mutual info of the Sepal Width and Petal Width features is: ', mutual_information(pmf_1_3, pmf_1, pmf_3))
-print('\nThe mutual info of the Petal Length and Petal Width features is: ', mutual_information(pmf_2_3, pmf_2, pmf_3))
-
-
-#print(data_matrix)
-#print(data_matrix_int)
-#print(pmf_0)
-#print(unique_rows_0)
-#print(pmf_1)
-#print(pmf_2)
-#print(pmf_3)
 
 
--- mutual_info.py
+++ mutual_info.py
@@ -1,24 +1,25 @@
 #!usr/bin/env python3
 
 
-# Returns the mutual information between two features, unique_rows contains the unique rows inside the two features,
-# joint is the joint pmf and marginal_x and marginal_y are the pmf of the features treated
-
-def mutual_information(joint, unique_rows, marginal_x, marginal_y):
+# Returns the mutual information between two features. Unique_rows contains the unique rows inside the two features
+# sub-matrix. Unique_rows_x and unique_rows_y contains the unique values inside the feature vector. Joint is the joint
+# pmf and marginal_x and marginal_y are the pmf of the features treated
+def mutual_information(joint, unique_rows, marginal_x, unique_rows_x, marginal_y, unique_rows_y):
 
     from numpy import log2 as log2
-
     mutual_info = 0                       # Mutual information of x and y
-    rows, columns = joint.shape           # Number of rows and columns of data_matrix
 
-    for i in range(0, rows):
+    # unique_rows, unique_rows_x and unique_rows_y have different dimensions, so a search for the correct index of these
+    # last two is needed
+    for i in range(0, len(joint)):
+        # The index of marginals needs to be searched according to the value inside the two features submatrix
         mutual_info += joint[i] * log2(joint[i] /
-                                       (marginal_x[search_pmf(unique_rows[0], marginal_x)] *
-                                        marginal_y[search_pmf(unique_rows[1], marginal_y)]))
-
+                                       (marginal_x[search_pmf(unique_rows[i][0], unique_rows_x)] *
+                                        marginal_y[search_pmf(unique_rows[i][1], unique_rows_y)]))
     return mutual_info
 
 
+# Searches inside unique_rows_x of a certain feature the value equals to a certain element of the unique_rows sub-matrix
 def search_pmf(unique_row_value, unique_rows_a):
     for i in range(0, len(unique_rows_a)):
         if unique_rows_a[i] == unique_row_value:
